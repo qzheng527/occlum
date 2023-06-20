381e579c1cf923362b0d057d7e5b4e6966799a1b
Qi Zheng <huaiqing.zq@antgroup.com>
2023-06-20 16:05:55 +0800

    Revert "[tools] Support configuring PKU in Occlum.json"
    This reverts commit b65cb4e0177648b666aa45cba8beae72642a7198.

--- tools/gen_internal_conf/src/main.rs
+++ tools/gen_internal_conf/src/main.rs
@@ -252,7 +252,6 @@ fn main() {
             ISVEXTPRODID_L: kss_tuple.2,
             ISVFAMILYID_H: kss_tuple.3,
             ISVFAMILYID_L: kss_tuple.4,
-            PKRU: occlum_config.metadata.pkru,
         };
         let enclave_config = serde_xml_rs::to_string(&sgx_enclave_configuration).unwrap();
         debug!("The enclave config:{:?}", enclave_config);
@@ -549,8 +548,6 @@ struct OcclumMetadata {
     enable_kss: bool,
     family_id: OcclumMetaID,
     ext_prod_id: OcclumMetaID,
-    #[serde(default)]
-    pkru: u32,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -614,7 +611,6 @@ struct EnclaveConfiguration {
     ISVEXTPRODID_L: u64,
     ISVFAMILYID_H: u64,
     ISVFAMILYID_L: u64,
-    PKRU: u32,
 }
 
 #[derive(Debug, PartialEq, Clone, Serialize)]
