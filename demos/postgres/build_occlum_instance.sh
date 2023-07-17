44873007dcfb84df7f575fe6a7aa21d7adb41551
Qi Zheng <huaiqing.zq@antgroup.com>
2023-07-17 10:28:39 +0800

    [demos] Enlarge TCS and add extra external mount for PG demo

--- demos/postgres/build_occlum_instance.sh
+++ demos/postgres/build_occlum_instance.sh
@@ -11,13 +11,14 @@ copy_bom -f ../pg.yaml --root image --include-dir /opt/occlum/etc/template
 # Fix " version `XCRYPT_2.0' not found " issue
 cp /lib/x86_64-linux-gnu/libcrypt.so.1 image/opt/occlum/glibc/lib/
 
-new_json="$(jq '.resource_limits.user_space_size = "8000MB" |
+new_json="$(jq '.resource_limits.user_space_size = "10000MB" |
                 .resource_limits.kernel_space_heap_size = "2000MB" |
                 .resource_limits.kernel_space_heap_max_size = "2000MB" |
-                .resource_limits.max_num_of_threads = 96 |
+                .resource_limits.kernel_space_stack_size ="4MB" |
+                .resource_limits.max_num_of_threads = 192 |
                 .env.default = [ "PATH=/usr/local/pgsql/bin" ] |
                 .env.default += ["PYTHONHOME=/opt/python-occlum"] |
-                .env.default += [ "LD_LIBRARY_PATH=/usr/local/pgsql/lib/:$LD_LIBRARY_PATH" ] |
+                .env.default += [ "LD_LIBRARY_PATH=/usr/local/pgsql/lib" ] |
                 .entry_points += [ "/usr/local/pgsql/bin" ]' Occlum.json)" && \
 echo "${new_json}" > Occlum.json
 
@@ -28,6 +29,15 @@ mkdir image/pg_data
 
 # Put PG data in external mount
 new_json="$(cat Occlum.json | jq '.mount+=[{"target": "/pg_data","type": "unionfs", "options": {"layers":[{"target": "/pg_data", "type": "sefs", "source": "../pg_data/lower"},{"target": "/pg_data", "type": "sefs", "source": "../pg_data/upper"}]}}]')" && \
+echo "${new_json}" > Occlum.json
+
+# Create external mount point
+rm -rf ../raw
+mkdir -p ../raw/upper && mkdir -p ../raw/lower
+mkdir image/raw
+
+# Add raw directory in external mount for other data
+new_json="$(cat Occlum.json | jq '.mount+=[{"target": "/raw","type": "unionfs", "options": {"layers":[{"target": "/raw", "type": "sefs", "source": "../raw/lower"},{"target": "/raw", "type": "sefs", "source": "../raw/upper"}]}}]')" && \
 echo "${new_json}" > Occlum.json
 
 # Set /tmp directory as ramfs
