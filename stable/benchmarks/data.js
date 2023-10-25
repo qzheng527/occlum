b76e871743efadb4e1d558c200141ceb92dbe7e7
github-action-benchmark <github@users.noreply.github.com>
2023-10-26 06:56:51 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for f3c624dc8920225f000c53e74e64d7bbd6c0b943

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698273435446,
+  "lastUpdate": 1698274611265,
   "repoUrl": "https://github.com/qzheng527/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -1772,6 +1772,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 2880,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Qi Zheng",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "Qi Zheng",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "id": "f3c624dc8920225f000c53e74e64d7bbd6c0b943",
+          "message": "[readthedoc] enable PDF format build",
+          "timestamp": "2023-10-18T09:58:55Z",
+          "url": "https://github.com/qzheng527/occlum/commit/f3c624dc8920225f000c53e74e64d7bbd6c0b943"
+        },
+        "date": 1698274610361,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 2877,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 2877,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
