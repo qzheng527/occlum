6a6954db579097096b57d9e8ebc737319dd1f8af
github-action-benchmark <github@users.noreply.github.com>
2023-11-02 06:39:15 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for f3c624dc8920225f000c53e74e64d7bbd6c0b943

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698533763754,
+  "lastUpdate": 1698878355176,
   "repoUrl": "https://github.com/qzheng527/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -1096,6 +1096,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Threads Maximum Latency",
             "value": 1814.19,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 450.77,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Qi Zheng",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "Qi Zheng",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "id": "f3c624dc8920225f000c53e74e64d7bbd6c0b943",
+          "message": "[readthedoc] enable PDF format build",
+          "timestamp": "2023-10-18T09:58:55Z",
+          "url": "https://github.com/qzheng527/occlum/commit/f3c624dc8920225f000c53e74e64d7bbd6c0b943"
+        },
+        "date": 1698878354184,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.56,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 81.42,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 1518.83,
             "unit": "ms",
             "extra": "max"
           },
