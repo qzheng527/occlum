2e7a3631faff037f7e18083d9933747ba4c07695
github-action-benchmark <github@users.noreply.github.com>
2023-11-16 06:31:15 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for b16c39970994c46dbfa1552fbd7d7ffc2eaf21f6

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700087041041,
+  "lastUpdate": 1700087475776,
   "repoUrl": "https://github.com/qzheng527/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -2150,6 +2150,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 2611,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "b16c39970994c46dbfa1552fbd7d7ffc2eaf21f6",
+          "message": "Bump version to 0.30.0",
+          "timestamp": "2023-09-20T06:12:02Z",
+          "url": "https://github.com/qzheng527/occlum/commit/b16c39970994c46dbfa1552fbd7d7ffc2eaf21f6"
+        },
+        "date": 1700087474865,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 2511,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 2511,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
