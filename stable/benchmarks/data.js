7eef3e7cf7ea70f2b514c566174803a5558459e3
github-action-benchmark <github@users.noreply.github.com>
2024-07-07 06:32:38 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 0c9a44fc60ed9b4db5f0c7009299760c8c865c2f

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720304720174,
+  "lastUpdate": 1720305158819,
   "repoUrl": "https://github.com/qzheng527/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -3264,6 +3264,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 458.96,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "0c9a44fc60ed9b4db5f0c7009299760c8c865c2f",
+          "message": "Add kernel_heap_monitor as the default feature",
+          "timestamp": "2024-05-28T11:08:29Z",
+          "url": "https://github.com/qzheng527/occlum/commit/0c9a44fc60ed9b4db5f0c7009299760c8c865c2f"
+        },
+        "date": 1720305157921,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.57,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 82.02,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 1604.94,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 442.73,
             "unit": "ms",
             "extra": "per95"
           }
